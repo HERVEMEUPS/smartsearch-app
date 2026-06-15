86860cf34f441b2bdc517f1dca1c65c2a0e9d591
HERVEMEUPS <rodymeups@gmail.com>
2026-06-15 14:07:36 +0100

    🔧 Fix: Accepter anciennes valeurs (Perdu/Trouvé) ET nouvelles (PERTE/DECOUVERTE) dans recherche

--- backend/src/services/declarationService.js
+++ backend/src/services/declarationService.js
@@ -60,7 +60,21 @@ class DeclarationService {
 
     // Filtres
     if (filters.type) {
-      query.type = filters.type;
+      // Accepter les anciennes et nouvelles valeurs
+      const typeMapping = {
+        'PERTE': ['PERTE', 'Perdu', 'perdu'],
+        'DECOUVERTE': ['DECOUVERTE', 'Trouvé', 'trouve', 'retrouvé']
+      };
+
+      if (typeMapping[filters.type]) {
+        query.type = { $in: typeMapping[filters.type] };
+      } else if (filters.type === 'Perdu' || filters.type === 'perdu') {
+        query.type = { $in: ['PERTE', 'Perdu', 'perdu'] };
+      } else if (filters.type === 'Trouvé' || filters.type === 'trouve' || filters.type === 'retrouvé') {
+        query.type = { $in: ['DECOUVERTE', 'Trouvé', 'trouve', 'retrouvé'] };
+      } else {
+        query.type = filters.type;
+      }
     }
 
     if (filters.typeDocument) {
